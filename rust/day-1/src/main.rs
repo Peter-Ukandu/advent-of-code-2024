326b467a05d8ae102bf35f5a24c9ceeb63ea8348
Peter Ukandu <76865134+plebius-lo3@users.noreply.github.com>
2024-12-01 16:29:21 +0100

    Solve day-1's first puzzle

--- rust/day-1/src/main.rs
+++ rust/day-1/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("src/input.txt").unwrap();
+    let numbers: Vec<i32> = input
+        .trim()
+        .split_ascii_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let mut list1: Vec<i32> = (0..numbers.len())
+        .step_by(2)
+        .map(|i| numbers[i])
+        .collect();
+
+    let mut list2: Vec<i32> = (1..numbers.len())
+        .step_by(2)
+        .map(|i| numbers[i])
+        .collect();
+
+    list1.sort();
+    list2.sort();
+
+    let result: i32 = (0..list1.len())
+        .map(|i| (list1[i] - list2[i]).abs())
+        .sum();
+
+    println!("{}", result);
 }
